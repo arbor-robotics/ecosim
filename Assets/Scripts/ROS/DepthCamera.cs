abf6c7d3e4cf55508976240bf67ee1c51a376acb
Will Heitman <will@heit.mn>
2024-10-09 10:46:16 -0400

    Add basic raycasting (improper transform)

--- Assets/Scripts/ROS/DepthCamera.cs
+++ Assets/Scripts/ROS/DepthCamera.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 // using std_msgs.msg;
 using Unity.Collections;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System;
 
 namespace ROS2
 {
@@ -17,7 +19,9 @@ namespace ROS2
         public string cameraName;
         public float hFovDegrees = 101;
         public float vFovDegrees = 68;
-        public float degreesPerPixel = 10;
+        // public float degreesPerPixel = 10;
+        public float widthPixels = 672;
+        public float heightPixels = 376;
 
 
         public new Camera camera;
@@ -42,14 +46,26 @@ namespace ROS2
 
         void Update()
         {
-            for (float i = 0; i < hFovDegrees; i += degreesPerPixel)
+            for (float i = 0; i < widthPixels; i++)
             {
-                for (float j = 0; j < vFovDegrees; j += degreesPerPixel)
+                for (float j = 0; j < heightPixels; j++)
                 {
+                    float x = i - (widthPixels / 2); // Range is now (-336 px, +336 px)
+                    x /= widthPixels / 2; // Range is now (-1, 1)
+                    x *= hFovDegrees / 2;
+                    float y = j - (heightPixels / 2);
+                    y /= heightPixels / 2; // Range is now (-1, 1)
+                    y *= vFovDegrees / 2; // Convert to degrees
+
+                    float ray_x = Mathf.Tan(Mathf.Deg2Rad * x);
+                    float ray_y = Mathf.Tan(Mathf.Deg2Rad * y);
+
+                    Vector3 ray_direction = new Vector3(ray_x, ray_y, 1f);
+
                     RaycastHit hit;
                     // Vector3 rayOrigin = rigidbody.position;
 
-                    if (Physics.Raycast(transform.position, Vector3.forward, out hit, maxRange))
+                    if (Physics.Raycast(transform.position, ray_direction, out hit, maxRange))
                     {
                         float wheelOffset = hit.distance - maxRange;
 
